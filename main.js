55cc5c00c6b32b88062014b04943ff22a53ab641
HenriqueGalvim <64918330+HenriqueGalvim@users.noreply.github.com>
2020-10-28 12:50:23 -0300

    Commit final

--- main.js
+++ main.js
@@ -25,25 +25,25 @@
   		window.open("https://henriquegalvim.github.io/Nivel1/");
       } 
 	    if (key == chave[2] || key == chave[3]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel2/");
 	      }
 	    if (key == chave[4] || key == chave[5]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel3/");
 	      }
 	    if (key == chave[6] || key == chave[7]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel4/");
 	      }
 	    if (key == chave[8] || key == chave[9] || key == chave[10]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel5/");
 	      }
 	    if (key == chave[11] || key == chave[12]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel6/");
 	      } 
 	    if (key == chave[13] || key == chave[14]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel7/");
 	      }
 	    if (key == chave[15] || key == chave[16]){
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel8/");
 	      }
   
   // Abaixo é pela parte de enviar pelo form
@@ -53,26 +53,26 @@
 	  	window.open("https://henriquegalvim.github.io/Nivel1/");
 	  }
 	  if (submit.value == chave[2] || submit.value == chave[3]){ //milly
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel2/");
 	  }
 	  if (submit.value == chave[4] || submit.value == chave[5]){ // ruby
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel3/
 	  }
 	  if (submit.value == chave[6] || submit.value == chave[7]){ // chico
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel4/");
 	  }
 	  if (submit.value == chave[8] || submit.value == chave[9] || submit.value == chave[10]){ // brabo
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel5/");
 	  }
 
 	  if (submit.value == chave[11] || submit.value == chave[12]){ // tt
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel6/");
 	  }
 	  if (submit.value == chave[13] || submit.value == chave[14]){ //a
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel7/");
 	  }
 	  if (submit.value == chave[15] || submit.value == chave[16]){ // hek
-	  	window.open("https://www.google.com.br/");
+	  	window.open("https://henriquegalvim.github.io/Nivel8/");
 	  }
 	  if(submit.value == ''){
 	  	window.alert("Nenhuma chave foi inserida")
